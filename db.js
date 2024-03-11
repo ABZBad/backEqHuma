88f4cf225f15a62ce00c6a4f731ef37241676d36
ABZBad <45019707+ABZBad@users.noreply.github.com>
2024-03-11 16:59:33 -0600

    cambio en usuarios a nube

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
+  user: 'fl0user',
+  //host: 'localhost',
+  host: 'ep-steep-water-a5hm4bm8.us-east-2.aws.neon.fl0.io',
   database: 'eqHuma',
-  password: 'Admin2024',
+  //password: 'Admin2024',
+  password: 'qL9cyadMT6jA',
   port: 5432,
 });
 
