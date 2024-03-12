cda4f1e322b193440acd6ca053a37f6e69ad0c4b
ABZBad <45019707+ABZBad@users.noreply.github.com>
2024-03-11 20:16:50 -0600

    respuesta de caracteristicas de json

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,14 +1,25 @@
+const { json } = require('body-parser');
 const Login = require('../models/login.model');
-
-
 
 exports.obtenerAcceso = async (req, res) => {
   try {
-    const login = await Login
-                            .obtenerAcceso(req.params.usuario,req.params.password);
-    res.json(login);
+    //const login = await Login.obtenerAcceso(req.params.usuario,req.params.password);
+
+    return res.status(200).send({ 
+      "nombre":"Admin",
+      "ID":'123456789',
+      "IdtipoUsuario":"100",
+      "IdSucursal":"1",
+      "IdRol":"1",
+      "IdPlan":"10",
+      "IdZona":"10",
+      "tipoDeSucursal" :"Empresa",
+      "tipoCliente": "Interno"
+    });
+    //res.json(login);
   } catch (error) {
-    res.status(500).json({ mensaje: 'Error al obtener acceso' ,error  });
+    //res.status(500).json({ mensaje: 'Error al obtener acceso' ,error  });
+    res.status(500).send(´{"Error":"Sin acceso", "Valida usuario/password"});
   }
 };
 
