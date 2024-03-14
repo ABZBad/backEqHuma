be6c1d5e192b743cec9fa1907b48c3de1105b64c
ABZBad <45019707+ABZBad@users.noreply.github.com>
2024-03-14 13:57:46 -0600

    login

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -5,7 +5,7 @@ exports.obtenerAcceso = async (req, res) => {
   try {
     //const login = await Login.obtenerAcceso(req.params.usuario,req.params.password);
 
-    return res.status(200).send({ 
+     res.status(200).send({ 
       "nombre":"Admin",
       "ID":'123456789',
       "IdtipoUsuario":"100",
