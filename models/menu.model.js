308bb6792fa833599f897a1657e4e42685206a2c
ABZBad <45019707+ABZBad@users.noreply.github.com>
2024-03-15 23:56:26 -0600

    add menu al

--- models/menu.model.js
+++ models/menu.model.js
@@ -3,7 +3,7 @@ class Menu {
   static async obtenerMenuPorPerfil(id) {
     const { rows } = await db.query('select * from  public.obtenerMenuPorPerfil($1)', [id]);
     console.log("salida Peril/obtenerPorId ---- $1 ", rows[0]);
-    return rows[0];
+    return rows;
   }
 }
 
