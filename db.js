af8a1fdd898c7d54da83eaf707cee32585b2755c
ABZBad <45019707+ABZBad@users.noreply.github.com>
2024-03-17 13:43:35 -0600

    add ignore

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'fl0user',
-  host: 'ep-steep-water-a5hm4bm8.us-east-2.aws.neon.fl0.io',
-  password: 'qL9cyadMT6jA',
-  ssl: {
-    rejectUnauthorized: true // Ignorar la verificación del certificado SSL
-  },
+  // user: 'fl0user',
+  // host: 'ep-steep-water-a5hm4bm8.us-east-2.aws.neon.fl0.io',
+  // password: 'qL9cyadMT6jA',
+  // ssl: {
+  //   rejectUnauthorized: true // Ignorar la verificación del certificado SSL
+  // },
   /// ambiente local
-  // user: 'postgres',
-  // host: 'localhost',
-  // password: 'Admin2024',
+  user: 'postgres',
+  host: 'localhost',
+  password: 'Admin2024',
 
   database: 'eqHuma',
   port: 5432,
