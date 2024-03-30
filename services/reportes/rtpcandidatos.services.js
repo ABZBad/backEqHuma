b8e536d7e83fb693c165b9d98238869d9c1da4dc
ABZBad <45019707+ABZBad@users.noreply.github.com>
2024-03-30 12:42:21 -0600

    reporte

---
+++ services/reportes/rtpcandidatos.services.js
@@ -0,0 +1,13 @@
+const Direccion = require('../models/direccion.model');
+
+class rptCandidatosPorEmpresaService {
+    static async crear(Id) {
+        try {
+            
+        } catch (error) {
+            throw new Error('Error al crear el empresa en Servicio/crear');
+        }
+    }
+
+    
+} module.exports = rptCandidatosPorEmpresaService;
